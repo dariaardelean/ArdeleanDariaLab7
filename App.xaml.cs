12d013ac8b223959bb858a276e0141153ef4e430
Daria <Daria@DARIA-LAPTOP>
2025-11-25 19:46:31 +0200

    Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,31 @@
-﻿namespace ArdeleanDariaLab7;
+﻿using System;
+using ArdeleanDariaLab7.Data;
+using System.IO;
+
+namespace ArdeleanDariaLab7;
 
 public partial class App : Application
 {
+	static ShoppingListDatabase database;
+
+	public static ShoppingListDatabase Database
+	{
+		get
+		{
+			if (database == null)
+			{
+				database = new
+ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+LocalApplicationData), "ShoppingList.db3"));
+            }
+			return database;
+		}
+	}
 	public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new NavigationPage(new AppShell());
+
+    }
 }
---
+++ ListPage.xaml.cs
@@ -0,0 +1,30 @@
+using System;
+using ArdeleanDariaLab7.Models;
+
+namespace ArdeleanDariaLab7;
+
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+}
